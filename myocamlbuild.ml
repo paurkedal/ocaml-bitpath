012f8f4ef41fdff695e079fad31a9de08927c855
Petter Urkedal <paurkedal@gmail.com>
2012-08-19 11:07:40 +0200

    Remove spaces at EOL.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,8 +1,8 @@
 (* OASIS_START *)
 (* OASIS_STOP *)
 
-let () = dispatch begin function 
-  | After_rules as e -> 
+let () = dispatch begin function
+  | After_rules as e ->
     flag ["doc"; "ocaml"] & S[A"-charset"; A"utf8"];
     dispatch_default e
   | e -> dispatch_default e
