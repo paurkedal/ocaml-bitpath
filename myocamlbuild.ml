3740ebd27107d48f439525f0804852dc813323ec
Petter A. Urkedal <paurkedal@gmail.com>
2016-02-17 23:14:00 +0100

    Build: Add DevFiles, tweak spaces, mark tests used.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,6 +1,8 @@
 (* OASIS_START *)
 (* OASIS_STOP *)
 
+let () = mark_tag_used "tests"
+
 let () = dispatch begin function
   | After_rules as e ->
     flag ["doc"; "ocaml"] & S[A"-charset"; A"utf8"];
