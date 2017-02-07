920483130ba88b684698c6a5b1a98a1f382fecc3
Petter A. Urkedal <paurkedal@gmail.com>
2017-02-07 21:09:23 +0100

    Rename src/ to lib/.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -7,8 +7,8 @@ let licenses = List.map Pkg.std_file ["COPYING.LESSER"; "COPYING"]
 
 let () = Pkg.describe ~licenses "bitpath" @@ fun c ->
   Ok [
-    Pkg.mllib "src/bitpath.mllib";
-    Pkg.clib "src/libbitpath_stubs.clib";
-    Pkg.lib "src/bitpath_prereq.h";
-    Pkg.lib "src/bitpath.h";
+    Pkg.mllib "lib/bitpath.mllib";
+    Pkg.clib "lib/libbitpath_stubs.clib";
+    Pkg.lib "lib/bitpath_prereq.h";
+    Pkg.lib "lib/bitpath.h";
   ]
