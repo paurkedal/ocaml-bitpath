1ae37179153408877e83fd2ba49e60b1026f7f81
Petter A. Urkedal <paurkedal@gmail.com>
2017-02-08 20:18:38 +0100

    Fix inclusion of C stubs in installed library.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,7 +1,17 @@
 open Ocamlbuild_plugin
 
+let clib ~dir name =
+  flag ["ocaml"; "library"; "link"; "byte"; "use_"^name]
+       (S [A "-dllib"; A("-l"^name)]);
+  flag ["ocaml"; "library"; "link"; "native"; "use_"^name]
+       (S [A "-cclib"; A("-l"^name)]);
+  flag ["link"; "ocaml"; "link_"^name] (A(dir^"/lib"^name^".a"));
+  dep ["link"; "ocaml"; "use_"^name^""] [dir^"/lib"^name^".a"]
+
 let () = dispatch @@ function
  | After_rules ->
     pdep ["c"; "compile"] "cdep" (fun dep -> [dep]);
-    pdep ["ocaml"; "link"] "linkdep" (fun dep -> [dep])
+    pdep ["ocaml"; "link"] "linkdep" (fun dep -> [dep]);
+    clib ~dir:"lib" "bitpath_stubs"
+
  | _ -> ()
